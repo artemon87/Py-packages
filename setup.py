f6eb7dde15ffc68abbf416e3bd06603a2085070e
artemon87 <artemkovtunenko@Artems-MBP.T-mobile.com>
2017-06-10 00:09:04 -0700

    second commit

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+setup(
+  name = 'toBase',
+  packages = ['toBase'], 
+  version = '0.1',
+  description = 'convert number to any base < 16',
+  author = 'Artem Kovtunenko',
+  author_email = 'kan.artemon@gmail.com',
+  url = 'https://github.com/artemon87/toBase', 
+  download_url = 'https://github.com/artemon87/toBase/archive/0.1.tar.gz', # I'll explain this in a second
+  keywords = ['testing', 'logging', 'example'],
+  classifiers = [],
+)
